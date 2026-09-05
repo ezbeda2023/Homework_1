298a0dc84377559de5c4b0b9e914981f02611e42
ezbeda2023 <ezbeda2023@fau.edu>
2026-09-05 18:38:59 -0400

    Update p3_Ethan_Zbeda.py

--- p3_Ethan_Zbeda.py
+++ p3_Ethan_Zbeda.py
@@ -1,15 +1,10 @@
-"""Find duplicated, non-overlapping substrings."""
-
-
 def find_dup_str(s, n):
-    """Return the first non-overlapping duplicated substring of length n."""
     if n <= 0:
         return ""
 
     for first_start in range(len(s) - n + 1):
         substring = s[first_start:first_start + n]
 
-        # The second occurrence must begin after the first one ends.
         for second_start in range(first_start + n, len(s) - n + 1):
             if s[second_start:second_start + n] == substring:
                 return substring
@@ -18,7 +13,6 @@ def find_dup_str(s, n):
 
 
 def find_max_dup(s):
-    """Return the longest non-overlapping duplicated substring in s."""
     for length in range(len(s) // 2, 0, -1):
         duplicated = find_dup_str(s, length)
         if duplicated != "":
